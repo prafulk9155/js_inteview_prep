22c0f711df18f90a2c47b4a67c7f51307c2565e2
Praful Kumar <prafulk9155@gmail.com>
2023-12-19 22:52:47 +0530

    day1

---
+++ home.js
@@ -0,0 +1,52 @@
+//  what is js ?
+// html+css --> used to make tatic page --> to make it interactive with user and to handle logic , iser inteaction and required functionality . js is used , co we can say that jtml and css is used as a skeleton and muscles but to mek it useable means , to intact with the real worls , js is used as a brain 
+
+
+// difference betn undefined   and null
+
+// let x=null;
+// console.log(x);  //variable is declared and assigned value null ( empty value ) -->  null
+
+// let y; // declared variable but not defined
+
+// console.log(y)
+
+
+//hoisting(only applicable with var , value called before dclared , automatically goes above any part means declaration as priority )
+// let a;
+// console.log(a);
+
+// console.log(b);
+// var b ;
+// b = 5;
+// console.log(b)
+
+// console.log(c);
+
+// let c;    >> error , not applicable wit let , const 
+
+// How do you declare variables in JavaScript? -->  let, var, const 
+
+let t1 =5 ;
+console.log(t1)
+// let t1=6;// error let& const not decalre more than 1 
+t1 =6;
+console.log(t1)
+
+
+const c1=10;
+
+console.log(c1)
+
+// c1=11;
+// console.log(c1)   // Assignment to constant variable.  error (cont value not change)
+
+var v1=0;
+console.log(v1)
+
+var v1 =100;
+console.log(v1) // redeclare , re assign and hoiting allowed
+
+
+
+
